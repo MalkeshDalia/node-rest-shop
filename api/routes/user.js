c5051d72406a50990c26d296fc5072cfd0f10a0e
Malkesh Dalia <malkesh2911@hotmail.com>
2022-10-14 16:11:52 +0530

    Added User Login and JWT Signing

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,8 +5,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
-// Sign-Up User Post
+// Post Request and Sign-Up for User
 router.post("/signup", (req, res, next) => {
   // Checking If Email already Exists then we will execute further process
   User.find({ email: req.body.email })
@@ -48,6 +49,52 @@ router.post("/signup", (req, res, next) => {
     });
 });
 
+// Post Request and Login for User
+router.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth Failed",
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (error, result) => {
+        if (error) {
+          return res.status(401).json({
+            message: "Auth Failed",
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h",
+            }
+          );
+          return res.status(200).json({
+            message: "Auth Successfully",
+            token: token,
+          });
+        }
+        res.status(401).json({
+          message: "Auth Failed",
+        });
+      });
+    })
+    .catch((error) => {
+      console.log("Something Went Wrong with User Delete", error);
+      res.status(500).json({
+        error: error,
+      });
+    });
+});
+
+// Delete Request
 router.delete("/:userId", (req, res, next) => {
   User.remove({ _id: req.params.userId })
     .exec()
